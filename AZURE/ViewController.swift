a3aa84659d44ea7cca8d74149937ac881334915b
malafa0101 <malafa01@gmail.com>
2018-10-07 17:11:46 +0600

    project

---
+++ AZURE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AZURE
+//
+//  Created by Zhanibek Santay on 10/7/18.
+//  Copyright © 2018 Zhanibek Santay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+
+
+}
+
